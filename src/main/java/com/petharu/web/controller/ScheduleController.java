b2649022e169c4495aed021f70c056ace7fdf42e
duswls3913 <hham3913@gmail.com>
2021-07-25 19:44:31 +0900

    하..정규표현식...주길까..이제 api만들어야댐

--- src/main/java/com/petharu/web/controller/ScheduleController.java
+++ src/main/java/com/petharu/web/controller/ScheduleController.java
@@ -1,12 +1,20 @@
 package com.petharu.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.petharu.web.entity.Schedule;
+import com.petharu.web.service.ScheduleService;
 
 @Controller
 @RequestMapping("/management/schedule/")
 public class ScheduleController {
+	
+	@Autowired
+	private ScheduleService service;
 	
 	@RequestMapping("calendar")
 	public String calendar() {
@@ -15,8 +23,24 @@ public class ScheduleController {
 	}
 
 	@PostMapping("reg")
-	public String reg() {
-		return "";
+	public String reg(String title, String datetime, String content,
+						int scheduleTypeId) {
+		Schedule schedule = new Schedule();
+		schedule.setTitle(title);
+		schedule.setContent(content);
+		schedule.setDateTime(datetime);
+		schedule.setMemberId(2);
+		schedule.setScheduleTypeId(scheduleTypeId);
+		
+		service.insert(schedule);
+		return "redirect:calendar";
+	}
+	
+	@GetMapping("del")
+	public String delete(int id) {
+		service.delete(id);
+		
+		return "redirect:calendar";
 	}
 
 
