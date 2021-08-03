08511db3e15e80319147b3f64349c7ad7a564bdd
duswls3913 <hham3913@gmail.com>
2021-08-03 15:19:55 +0900

    member관련 class 생성

---
+++ src/main/java/com/petharu/web/dao/mybatis/MyBatisMemberDao.java
@@ -0,0 +1,27 @@
+package com.petharu.web.dao.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.petharu.web.dao.MemberDao;
+import com.petharu.web.entity.Member;
+
+public class MyBatisMemberDao implements MemberDao {
+	
+	private SqlSession sqlSession;
+	private MemberDao memberMapper;
+	
+	@Autowired
+	public MyBatisMemberDao(SqlSession sqlSession) {
+		// TODO Auto-generated constructor stub
+		this.sqlSession=sqlSession;
+		memberMapper = sqlSession.getMapper(MemberDao.class);
+	}
+
+	@Override
+	public int insert(Member member) {
+		// TODO Auto-generated method stub
+		return memberMapper.insert(member);
+	}
+
+}
